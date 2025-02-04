65488cce09aa7cb8c3917763ef80b48de3fe0151
codekookiz <codekookiz@gmail.com>
2025-02-04 15:01:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def main():
         
         st.subheader('🎨 클러스터 시각화')
         fig2 = plt.figure()
-        palette = sns.color_palette("tab10", k)
+        palette = sb.color_palette("tab10", k)
         sb.scatterplot(x=df_new.iloc[:, 0], y=df_new.iloc[:, 1], hue=df['Group'], palette=palette, legend='full')
         plt.xlabel(selected_columns[0])
         plt.ylabel(selected_columns[1] if len(selected_columns) > 1 else 'Feature 2')
